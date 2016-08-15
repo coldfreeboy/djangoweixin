3c1a824fa4a0e79f0232c06f72f0cc01f10b0972
dang <270768983@qq.com>
2016-08-15 14:32:26 +0800

    接口配置

--- weixin/app/views.py
+++ weixin/app/views.py
@@ -1,5 +1,24 @@
+#!coding:utf-8
 from django.shortcuts import render,HttpResponse
+import hashlib
 
 # Create your views here.
 def token(request):
-    return HttpResponse('<h1>ok<h1>')
+    # 与微信验证
+    # 将timestamp，noce，token按字典排序
+    timestamp = request.GET.get('timestamp')
+    nonce     = request.GET.get('nonce')
+    taken     = "dangweiwu"
+    signature = request.GET.get('signature')
+    
+    # 排序后进行sha1加密
+    l = sorted([timestamp,nonce,taken])
+    tmpstr = "".join(l)
+
+    sha1 = hashlib.sha1()
+    tmpstr = sha1.update(tmpstr).hexdigest()
+    # 验证成功返回echostr
+    if(signature == tmpstr):
+        response = request.GET.get('echostr')
+
+    return HttpResponse(response)
