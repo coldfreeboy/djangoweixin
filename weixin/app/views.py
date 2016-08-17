7b103e52b9f4ed707462f8862976da48989e624a
dang <270768983@qq.com>
2016-08-18 00:28:06 +0800

    views_修改

--- weixin/app/views.py
+++ weixin/app/views.py
@@ -31,20 +31,14 @@ def responseMsg(request):
             creatTime = str(int(time.time()))
             msgType = 'text'
             content = "欢迎订阅"
-            template = """<xml>
-
-            <ToUserName><![CDATA[%s]]></ToUserName>
-
-            <FromUserName><![CDATA[%s]]></FromUserName>
-
-            <CreateTime>%s</CreateTime>
-
-            <MsgType><![CDATA[%s]]></MsgType>
-
-            <Content><![CDATA[%s]]></Content>
-
-            </xml>"""%(toUser,fromUser,creatTime,msgType,content)
-            return template
+            template = "<xml>"+\
+            "<ToUserName><![CDATA[%s]]></ToUserName>"+\
+            "<FromUserName><![CDATA[%s]]></FromUserName>"+\
+            "<CreateTime>%s</CreateTime>"+\
+            "<MsgType><![CDATA[%s]]></MsgType>"+\
+            "<Content><![CDATA[%s]]></Content>"+\
+            "</xml>"
+            return template%(toUser,fromUser,creatTime,msgType,content)
 
 
 @csrf_exempt
